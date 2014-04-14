c059826b84cc1c352d05bf8212c995404acb1692
Shane Cowen <dopefish89@gmail.com>
2014-04-14 11:25:15 -0700

    Third commit

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -7,19 +7,19 @@ int main(int argc, char *argv[])
     if(argc < 2) {
         printf("You require an argument.\n");
         return 1;
-    } else {
-        count = atoi(argv[1]);
-    }
+    } 
+    count = atoi(argv[1]);
+    
     for(i = 1; i <= count; i++) {
         if(i % 3 == 0 && i % 5 == 0) {
             printf("fizzbuzz\n");
         } else if(i % 3 == 0) {
             printf("fizz\n");
-        } else if(i % 5 == 0) {
+        } else if(i % 5 == 0) { 
             printf("buzz\n");
         } else {
             printf("%d\n", i);
         }
     }
     return 0;
-}
+}   
